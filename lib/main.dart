0cf4851a225f6d13f1e56b8f8cb9c59fd91ac284
Ahmed <ahmedmohsen087@gmail.com>
2022-08-16 12:54:48 +0200

    initialized project add empty home screen

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:islami/home_screen.dart';
+
+void main (){
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: HomeScreen.routeName,
+      routes: {
+        HomeScreen.routeName:(context) =>HomeScreen(),
+      },
+    );
+  }
+}
